49cbe768dbf5abe33607b33e109852c4c306e593
kazybek <kanalkazybek@gmail.com>
2026-03-13 15:47:53 +0500

    hm4

--- src/com/narxoz/rpg/battle/RaidEngine.java
+++ src/com/narxoz/rpg/battle/RaidEngine.java
@@ -14,20 +14,45 @@ public class RaidEngine {
     }
 
     public RaidResult runRaid(CombatNode teamA, CombatNode teamB, Skill teamASkill, Skill teamBSkill) {
-        // TODO: Validate inputs (null checks, alive checks, required skills).
-        // TODO: Implement round-based simulation:
-        // 1) Team A casts on Team B
-        // 2) Team B casts on Team A (if still alive)
-        // 3) Track rounds and log each step
-        // 4) Stop when one team is defeated (or max rounds reached)
-        //
-        // Optional extension:
-        // Use random for critical strikes or other deterministic events.
-        // Example: boolean critA = random.nextInt(100) < 10;
         RaidResult result = new RaidResult();
-        result.setRounds(0);
-        result.setWinner("TBD");
-        result.addLine("TODO: implement raid simulation");
+
+        // check
+        if (teamA == null || teamB == null || !teamA.isAlive() || !teamB.isAlive()) {
+            result.setWinner("No Battle (Invalid teams)");
+            result.setRounds(0);
+            return result;
+        }
+
+        int rounds = 0;
+        result.addLine("RAID STARTED:" + teamA.getName() + " vs " + teamB.getName() + " ===");
+
+        while (teamA.isAlive() && teamB.isAlive()) {
+            rounds++;
+            result.addLine("\n[Раунд " + rounds + "]");
+
+            // Team A атакует
+            result.addLine(teamA.getName() + " use " + teamASkill.getSkillName() + "!");
+            teamASkill.cast(teamB); // Скилл арқылы урон береміз
+            result.addLine(">>> У " + teamB.getName() + " HP: " + teamB.getHealth());
+
+
+            if (!teamB.isAlive()) break;
+
+            // тиим б ответить
+            result.addLine(teamB.getName() + " use " + teamBSkill.getSkillName() + "!");
+            teamBSkill.cast(teamA);
+            result.addLine(">>> У " + teamA.getName() + "HP " + teamA.getHealth());
+        }
+
+        // сохранить
+        result.setRounds(rounds);
+        if (teamA.isAlive()) {
+            result.setWinner(teamA.getName());
+        } else {
+            result.setWinner(teamB.getName());
+        }
+
+        result.addLine("\n=== REID. Победитель: " + result.getWinner() + " ===");
         return result;
     }
 }
